4cc667f50d1a67066fec834a3375ee9ed1320e56
crylonblue <till.kemper@yahoo.de>
2026-01-18 03:15:23 +0100

    Add remote image pattern configuration for external CDN in Next.js setup
    - Updated the Next.js configuration to include a new remote pattern for images, allowing images to be fetched from 'cdn.blitzrechnung.de' over HTTPS. This change enhances the application's ability to serve images from external sources, improving performance and flexibility in content delivery.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.blitzrechnung.de',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
